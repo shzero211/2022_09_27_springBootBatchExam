8eb3998725c2d3f8c32310cf107fc099a7f5e308
SangHun <shzero211@naver.com>
2022-09-26 09:50:07 +0900

    Feat:HelloWorldJob 등록

---
+++ src/main/java/com/ll/exam/spring_batch_exam/job/HelloWorldJobConfig.java
@@ -0,0 +1,47 @@
+package com.ll.exam.spring_batch_exam.job;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloWorldJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob")
+                .start(helloWorldStep1())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+    @Bean
+    public Step helloWorldStep1() {
+        return stepBuilderFactory.get("helloWorldStep1")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return (contribution, chunkContext) -> {
+            System.out.println("헬로월드!");
+
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+
+}
