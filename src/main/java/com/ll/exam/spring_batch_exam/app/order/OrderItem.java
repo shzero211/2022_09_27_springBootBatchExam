d5ae0302d32bf10342257539f80f58f57d941a6d
SangHun <shzero211@naver.com>
2022-09-27 10:08:12 +0900

    Feat:주문 수수료,결제금액,환불금액,결제여부 추가

--- src/main/java/com/ll/exam/spring_batch_exam/app/order/OrderItem.java
+++ src/main/java/com/ll/exam/spring_batch_exam/app/order/OrderItem.java
@@ -31,6 +31,11 @@ public class OrderItem extends BaseEntity {
     private int salePrice;//실제판매가
     private int wholesalePrice;//도매가
 
+    private int pgFee;//결제대행사 수수료
+    private int payPrice;//결제금액
+    private int refundPrice;//환불금액
+    private int refundQuantity;//환불할 개수
+    private boolean isPaid;//결제 여부
 
     public OrderItem(ProductOption productOption,int quantity){
         this.price=productOption.getPrice();
