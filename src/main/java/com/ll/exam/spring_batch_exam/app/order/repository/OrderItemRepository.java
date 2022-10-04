e693124cc2364be01df0352f55ae3da97458ca1e
SangHun <shzero211@naver.com>
2022-10-04 10:01:27 +0900

    Feat: 주문품목의 id가 6보다 작은 데이터만 정산데이터 생성

--- src/main/java/com/ll/exam/spring_batch_exam/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/ll/exam/spring_batch_exam/app/order/repository/OrderItemRepository.java
@@ -1,7 +1,10 @@
 package com.ll.exam.spring_batch_exam.app.order.repository;
 
 import com.ll.exam.spring_batch_exam.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+    Page<OrderItem> findAllByIdLessThan(Long id, Pageable pageable);
 }
